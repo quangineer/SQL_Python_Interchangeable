c1c5f19929c1ee9a058ed2ae59b57ffe15921c3d
Quan Nguyen <quangineer@gmail.com>
2019-06-11 08:58:53 -0700

    while loop

---
+++ python_1.py
@@ -0,0 +1,21 @@
+# # The while Loop:
+# i = 1
+# while i < 6:
+#     print(i)
+#     i += 1            # 1,2,3,4,5
+
+# The break Statement:
+# i = 1
+# while i < 6:
+#     print (i)
+#     if i == 3:
+#         break
+#     i += 1           # 1,2,3
+
+# The continue Statement:
+i = 0
+while i < 6:
+    i += 1
+    if i == 3:
+        continue
+    print (i)
